194ecff43a6ca97b0997275507d099c429a85a64
Vivian.Potts <luc.potts@outlook.com>
2025-01-29 11:43:13 +0000

    1

---
+++ src/main/java/com/example/CinemaTicketServer/CinemaTicketServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.CinemaTicketServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CinemaTicketServer/CinemaTicketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CinemaTicketServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
