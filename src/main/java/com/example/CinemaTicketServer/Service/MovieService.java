f52940573418748e1c5bff5d7b3ba1390afdfe15
EfiW <efi.whiteley@gmail.com>
2025-02-06 09:52:00 +0000

    Create MovieService & ShowingService

---
+++ src/main/java/com/example/CinemaTicketServer/Service/MovieService.java
@@ -0,0 +1,26 @@
+package com.example.CinemaTicketServer.Service;
+
+import com.example.CinemaTicketServer.Model.Movie;
+import com.example.CinemaTicketServer.Repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepo;
+
+
+    void saveMovie(Movie movie){
+        movieRepo.save(movie);
+    }
+
+    Optional<Movie> getMovie(int query){
+        return movieRepo.findById(query);
+
+    }
+
+}
--- src/main/java/com/example/CinemaTicketServer/Service/ShowingService.java
+++ src/main/java/com/example/CinemaTicketServer/Service/ShowingService.java
@@ -1,29 +1,14 @@
 package com.example.CinemaTicketServer.Service;
 
-import com.example.CinemaTicketServer.Model.Movie;
 import com.example.CinemaTicketServer.Model.Showing;
-import com.example.CinemaTicketServer.Repository.MovieRepository;
 import com.example.CinemaTicketServer.Repository.ShowingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.Optional;
 
 @org.springframework.stereotype.Service
 public class Service {
 
     @Autowired
     private ShowingRepository showingRepo;
-    @Autowired
-    private MovieRepository movieRepo;
-
-    void saveMovie(Movie movie){
-        movieRepo.save(movie);
-    }
-
-    Optional<Movie> getMovie(int query){
-        return movieRepo.findById(query);
-        
-    }
 
 
     void saveBooking(Showing showing){
