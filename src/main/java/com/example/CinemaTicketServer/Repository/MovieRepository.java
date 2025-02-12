7d1a6048065f04a9790996215674c8d6467dbe4f
Vivian.Potts <luc.potts@outlook.com>
2025-02-12 10:28:43 +0000

    Added basic repository for interacting with api database

--- src/main/java/com/example/CinemaTicketServer/Repository/MovieRepository.java
+++ src/main/java/com/example/CinemaTicketServer/Repository/MovieRepository.java
@@ -2,9 +2,9 @@ package com.example.CinemaTicketServer.Repository;
 
 import com.example.CinemaTicketServer.Model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
-
-@org.springframework.stereotype.Repository
+@Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
 }
