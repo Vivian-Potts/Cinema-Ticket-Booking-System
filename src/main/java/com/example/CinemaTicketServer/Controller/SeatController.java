507c251e89845f86efc46cc74f79229c1bcc7feb
EfiW <efi.whiteley@gmail.com>
2025-03-27 12:12:30 +0000

    Endpoints 1.2

--- src/main/java/com/example/CinemaTicketServer/Controller/SeatController.java
+++ src/main/java/com/example/CinemaTicketServer/Controller/SeatController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,10 +15,10 @@ public class SeatController {
     @Autowired
     SeatService seatService;
 
-    @PostMapping("/book")
-    public ResponseEntity<String> bookSeat(@PathVariable int seatNumber, @PathVariable int showingId){
+    @PostMapping("/book/{id}")
+    public ResponseEntity<String> bookSeat(@PathVariable int id, @RequestParam int seatNumber){
 
-        return switch (seatService.bookSeat(seatNumber, showingId)) {
+        return switch (seatService.bookSeat(seatNumber, id)) {
             case (0) -> ResponseEntity.ok("Seat Booked");
             case (1) -> ResponseEntity.badRequest().body("Movie id could not be found in database");
             case (2) -> ResponseEntity.badRequest().body("Seats range from 1 to 300");
